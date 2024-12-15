10e47a4cdab7a1fe26ff5dcd6b77a1487c735b4b
Diell <budimadiell@gmail.com>
2024-12-15 14:11:57 +0100

    Made Navigation Bar Responsive

--- Components/Navigation/index.js
+++ Components/Navigation/index.js
@@ -8,7 +8,7 @@ class NavigationComponent extends HTMLElement {
             <header>
         <nav class="navBar">
             <div class="navBarLeft">
-                <img src="../../Assets/Logo.svg" alt="">
+                <img src="../../Assets/General/Logo.svg" alt="">
                 <ul class="navBarList">
                     <li class="dropDownListItem">Home</li>
                     <li class="dropDownListItem">Listings</li>
@@ -19,20 +19,21 @@ class NavigationComponent extends HTMLElement {
                     <li class="nothingListItem">Contact</li>
                     </span>
                     <div class="contactNumber">
-                        <img src="../../Assets/Phone.svg" alt="">
+                        <img src="../../Assets/NavigationAssets/Phone.svg" alt="">
                         <p>+75 123 456 789</p>
                     </div>
                 </ul>
             </div>
             <div class="navBarRight">
                 <div class="signIn">
-                    <img src="../../Assets/UserIcon.svg" alt="">
+                    <img src="../../Assets/NavigationAssets/UserIcon.svg" alt="">
                     <p>Sign In</p>
                 </div>
                 <button class="submitListingButton">
                     Submit Listing
                 </button>
             </div>
+            <img class="hamburgerIcon" src="../../Assets/NavigationAssets/HamburgerIcon.svg" alt="">
         </nav>
     </header>
         `
